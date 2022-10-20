a047291945c7560ba78c6659ab3b221d20d450f7
Rajat376 <89566014+Rajat376@users.noreply.github.com>
2022-10-20 12:02:10 +0530

    minor path changes in auth.js

--- Frontend/testapi/pages/auth.js
+++ Frontend/testapi/pages/auth.js
@@ -2,7 +2,7 @@ import Script from 'next/script'
 export default function Login()
 {
 return <div className='logpg'><center><div className="authbody" id='authbody'>
-    <form action="localhost:5000/api/users" method ="post" id="auth1">
+    <form action="https://localhost:5000/api/users" method ="post" id="auth1">
         <label className='lab1'>Name</label><br></br>
         <input type="text" placeholder="Enter Your Name" name="name" className="name"></input><br></br>
         <label className='lab1'>Email</label><br></br>
@@ -15,7 +15,7 @@ return <div className='logpg'><center><div className="authbody" id='authbody'>
         <p className="linktext" id="link1">Already registered user ? Sign in.</p>
         
     </form>
-    <form action="localhost:5000/api/users/login" id="auth2" method ="post">
+    <form action="https://localhost:5000/api/users/login" id="auth2" method ="post">
         <label className='lab1'>Email</label><br></br>
         <input type="email" placeholder="xyz@gamil.com" name="email" className="email"></input><br></br>
         <label className='lab1'>Password</label><br></br>
