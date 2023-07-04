9570da31400488678d7bed1f169375e531f06908
Rajat376 <89566014+Rajat376@users.noreply.github.com>
2023-07-04 12:33:39 +0530

    one command

--- Frontend/testapi/pages/form2.js
+++ Frontend/testapi/pages/form2.js
@@ -1,77 +1,80 @@
+import React, { useState } from 'react';
 import swal from 'sweetalert';
 
 export default function Form2() {
-  // Handles the submit event on form submit.
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const handleSubmit = async (event) => {
-    // Stop the form from submitting and refreshing the page.
-    event.preventDefault()
+    event.preventDefault();
 
-    // Get data from the form.
     const data = {
-      
-      email: event.target.email.value,
-      password:event.target.password1.value,
-    }
+      email: email,
+      password: password,
+    };
 
-    // Send the data to the server in JSON format.
-    const JSONdata = JSON.stringify(data)
-    //alert(JSONdata)
-//alert(JSONdata);     // API endpoint where we send form data.
-    const endpoint = '/login'
-    
-    // Form the request for sending data to the server.
+    const JSONdata = JSON.stringify(data);
+    const endpoint = '/login';
     const options = {
-      // The method is POST because we are sending data.
       method: 'POST',
-      // Tell the server we're sending JSON.
       headers: {
         'Content-Type': 'application/json',
       },
-      // Body of the request is the JSON data we created above.
       body: JSONdata,
-    }
+    };
 
-    // Send the form data to our forms API on Vercel and get a response.
-   const response = await fetch(endpoint, options)
- 
-    // Get the response data from server as JSON.
-    // If server returns the name submitted, that means the form works.
-   const result = await response.json()
-    if(`${result.name}`=="undefined")
-    { //sweet alert to be used later
-      
+    const response = await fetch(endpoint, options);
+    const result = await response.json();
+
+    if (typeof result.name === 'undefined') {
       swal({
-        title: "Invalid user name or password",
-        
-        icon: "error",
-        button: "continue",
+        title: 'Invalid username or password',
+        icon: 'error',
+        button: 'continue',
       });
+    } else {
+      localStorage.setItem('name', result.name);
+      localStorage.setItem('_id', result._id);
+      localStorage.setItem('email', result.email);
+      localStorage.setItem('token', result.token);
+      localStorage.setItem('isAdmin', result.isAdmin);
+      window.location.replace('../main');
     }
-    else
-    { setCookie('token',`${result.token}`,1000);
-      localStorage.setItem('name',`${result.name}`)
-    localStorage.setItem('_id',`${result._id}`)
-    localStorage.setItem('email',`${result.email}`)
-    localStorage.setItem('token',`${result.token}`)
-    localStorage.setItem('isAdmin',`${result.isAdmin}`)
-      window.location.replace("../main");
+  };
 
-    }
-  }
   return (
-    // We pass the event to the handleSubmit() function on submit.
-   
-    <form  onSubmit={handleSubmit} id="auth2" method ="post">
-    <label className='lab1'>Email</label><br></br>
-    <input type="email" placeholder="xyz@gamil.com" name="email" id="email" className="email"></input><br></br>
-    <label className='lab1'>Password</label><br></br>
-    <input type="password" placeholder="*********" name="password1" id="password1" className="password"></input>
-    <img className='passvis1'src='./images/passvis.png' id='passvis1'></img><br></br>
-    <button type="submit" value="submit" className="btn1">
+    <form onSubmit={handleSubmit} id="auth2" method="post">
+      <label className="lab1">Email</label>
+      <br />
+      <input
+        type="email"
+        placeholder="xyz@gamil.com"
+        name="email"
+        id="email1"
+        className="email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <br />
+      <label className="lab1">Password</label>
+      <br />
+      <input
+        type="password"
+        placeholder="*********"
+        name="password1"
+        id="password1"
+        className="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <img className="passvis1" src="./images/passvis.png" id="passvis1" />
+      <br />
+      <button type="submit" value="submit" className="btn1">
         Login
-    </button>
-    <p className="linktext" id="link2">Not registered yet ? Sign up.</p>
-</form>
-  
-  )
+      </button>
+      <p className="linktext" id="link2">
+        Not registered yet? Sign up.
+      </p>
+    </form>
+  );
 }
